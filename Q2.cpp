e0297a029dfe71b16139d9ad385b7b1264c6b9a4
mems250005018 <mems250005018@iiti.ac.in>
2025-10-17 16:00:37 +0530

    Update Q2.cpp

--- Q2.cpp
+++ Q2.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+struct Color { 
+    int r, g, b; 
+};
+
+int main() {
+    Color c[5] = { 
+    {10,20,30}, 
+    {200,150,100},
+    {50,60,70}, 
+    {255,255,0}, 
+    {128,64,255} 
+    };
+    //This will invert the first 3
+    for (int i = 0; i < 3; ++i) {
+        c[i].r = 255 - c[i].r;
+        c[i].g = 255 - c[i].g;
+        c[i].b = 255 - c[i].b;
+    }
+    //this will print the color structures
+    for (int i = 0; i < 5; ++i) {
+        cout << "Color " << i+1 << ": " << c[i].r << "," << c[i].g << "," << c[i].b<<endl;
+    }
+    return 0;
+}
